e6896fc6cf0ffa540548967f51a795cf6e61c4ee
nonyx777 <nahomretebo@gmail.com>
2025-04-15 14:13:47 +0300

    tilting implemented

--- Script/character.gd
+++ Script/character.gd
@@ -20,13 +20,16 @@ var key_direction: Vector3
 # Linear Movement
 @export var force: float = 20
 var acceleration: Vector3
+var prev_acceleration: Vector3
 var velocity: Vector3
 var force_vec: Vector3
 var drag: float = 0.9
 var snappiness: float = 3.0
 var target_angle: float
-var desired_rotation: Basis
-var temporary_transform: Transform3D
+var desired_rotation1: Quaternion
+var desired_rotation2: Quaternion
+var direction_rotation: Quaternion
+var tilt_rotation: Quaternion
 
 func process_input() -> void:
 	if Input.is_action_pressed("Forward"):
@@ -54,10 +57,8 @@ func movementOrientation(delta: float) -> void:
 	var camera_yaw: float = camera.transform.basis.get_euler().y
 	target_angle = atan2(key_direction.x, -key_direction.z) + camera_yaw
 	# Smoothly rotate to target rotation
-	desired_rotation = Basis(characterBody.transform.basis.y, target_angle)
-	temporary_transform = characterBody.transform
-	temporary_transform.basis = temporary_transform.basis.slerp(desired_rotation, snappiness * delta)
-	characterBody.transform = temporary_transform
+	desired_rotation1 = Quaternion(Vector3.UP, target_angle)
+	characterBody.transform.basis = Basis(characterBody.transform.basis.get_rotation_quaternion().slerp(desired_rotation1, snappiness * delta))
 	force_vec = Vector3(sin(target_angle), 0.0, cos(target_angle))
 
 func movement(delta: float) -> void:
@@ -66,6 +67,18 @@ func movement(delta: float) -> void:
 	velocity += acceleration * delta
 	velocity *= drag
 
+func tilt(delta: float) -> void:
+	# cross product between local y and acceleraction vector
+	var norm_local_y: Vector3 = characterBody.transform.basis.y.normalized()
+	var norm_acc: Vector3 = acceleration.normalized()
+	var rotation_axis: Vector3 = norm_local_y.cross(norm_acc)
+	if rotation_axis == Vector3.ZERO:
+		var t = Quaternion().from_euler(Vector3(0.0, desired_rotation1.get_euler().y, 0.0))
+		characterBody.transform.basis = Basis(characterBody.transform.basis.get_rotation_quaternion().slerp(t, snappiness * delta))
+		return
+	
+	characterBody.transform.basis = characterBody.transform.basis.slerp(characterBody.transform.basis.rotated(rotation_axis.normalized(), deg_to_rad(10.0)), snappiness * delta)
+
 func _ready() -> void:
 	pass
 
@@ -73,5 +86,6 @@ func _process(delta: float) -> void:
 	process_input()
 	movementOrientation(delta)
 	movement(delta)
+	tilt(delta)
 	characterBody.velocity = velocity
 	characterBody.move_and_slide()
